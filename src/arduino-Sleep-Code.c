79749c84e504bb14e80ead47001531e83038fac5
el-ghoujdami <hamzaelghoujdami@gmail.com>
2017-02-13 15:24:20 +0100

    add-arduino-sleep-code

---
+++ src/arduino-Sleep-Code.c
@@ -0,0 +1,128 @@
+/*
+ * Sketch for testing sleep mode with wake up on WDT.
+ * Donal Morrissey - 2011.
+ *
+ */
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
+
+#define LED_PIN (13)
+
+volatile int f_wdt=1;
+
+/***************************************************
+ *  Name:        ISR(WDT_vect)
+ *
+ *  Returns:     Nothing.
+ *
+ *  Parameters:  None.
+ *
+ *  Description: Watchdog Interrupt Service. This
+ *               is executed when watchdog timed out.
+ *
+ ***************************************************/
+ISR(WDT_vect)
+{
+  if(f_wdt == 0)
+  {
+    f_wdt=1;
+  }
+  else
+  {
+    Serial.println("WDT Overrun!!!");
+  }
+}
+
+/***************************************************
+ *  Name:        enterSleep
+ *
+ *  Returns:     Nothing.
+ *
+ *  Parameters:  None.
+ *
+ *  Description: Enters the arduino into sleep mode.
+ *
+ ***************************************************/
+void enterSleep(void)
+{
+  set_sleep_mode(SLEEP_MODE_PWR_SAVE);   /* EDIT: could also use SLEEP_MODE_PWR_DOWN for lowest power consumption. */
+  sleep_enable();
+
+  /* Now enter sleep mode. */
+  sleep_mode();
+
+  /* The program will continue from here after the WDT timeout*/
+  sleep_disable(); /* First thing to do is disable sleep. */
+
+  /* Re-enable the peripherals. */
+  power_all_enable();
+}
+
+/***************************************************
+ *  Name:        setup
+ *
+ *  Returns:     Nothing.
+ *
+ *  Parameters:  None.
+ *
+ *  Description: Setup for the serial comms and the
+ *                Watch dog timeout.
+ *
+ ***************************************************/
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Initialising...");
+  delay(100); //Allow for serial print to complete.
+
+  pinMode(LED_PIN,OUTPUT);
+
+  /*** Setup the WDT ***/
+
+  /* Clear the reset flag. */
+  MCUSR &= ~(1<<WDRF);
+
+  /* In order to change WDE or the prescaler, we need to
+   * set WDCE (This will allow updates for 4 clock cycles).
+   */
+  WDTCSR |= (1<<WDCE) | (1<<WDE);
+
+  /* set new watchdog timeout prescaler value */
+  WDTCSR = 1<<WDP0 | 1<<WDP3; /* 8.0 seconds */
+
+  /* Enable the WD interrupt (note no reset). */
+  WDTCSR |= _BV(WDIE);
+
+  Serial.println("Initialisation complete.");
+  delay(100); //Allow for serial print to complete.
+}
+
+/***************************************************
+ *  Name:        enterSleep
+ *
+ *  Returns:     Nothing.
+ *
+ *  Parameters:  None.
+ *
+ *  Description: Main application loop.
+ *
+ ***************************************************/
+void loop()
+{
+  if(f_wdt == 1)
+  {
+    /* Toggle the LED */
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+
+    /* Don't forget to clear the flag. */
+    f_wdt = 0;
+
+    /* Re-enter sleep mode. */
+    enterSleep();
+  }
+  else
+  {
+    /* Do nothing. */
+  }
+}
